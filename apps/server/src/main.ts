0efbe91f25aeb868b085672fd2cf547df960916e
selim-kerimov <selimkerimovdv@gmail.com>
2026-05-07 19:54:24 +0500

    feat: add swagger config

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,13 +2,26 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack monorepo server')
+    .setDescription('Description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'openapi-json',
+  });
+
   await app.listen(port);
-  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
